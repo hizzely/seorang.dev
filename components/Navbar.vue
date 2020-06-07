f651f4104e8ee09447ef0d4e34b4878be3715540
Fajar Ru <kzofajar@gmail.com>
2020-06-07 18:32:05 +0700

    Add navbar component and Updated index page

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="bg-white mb-4" role="navigation">
+    <div class="container py-8 mx-auto px-8 md:px-0 lg:w-2/3 xl:max-w-3xl flex flex-wrap items-center justify-between">
+      <div id="navbar-brand" class="mr-4 mb-4 text-center w-full block md:mb-0 md:text-left md:w-auto md:inline-block">
+        <a href="/">
+          <img src="https://avatars3.githubusercontent.com/u/24291960?s=64" class="inline-block w-8 rounded-full mr-1">
+          <span class="text-lg text-gray-700 font-semibold align-middle">Rohmad Fajarudin</span>
+        </a>
+      </div>
+      <div class="text-gray-700 text-center w-full block md:text-left md:w-auto md:inline-block">
+        <a href="/about" class="px-4">About</a>
+        <a href="/snippets" class="px-4">Snippets</a>
+        <a href="/blog" class="px-4">Tulisan</a>
+        <a href="#" class="px-4 border border-gray-400 rounded-md py-1">Portfolio</a>
+      </div>
+    </div>
+  </nav>
+</template>

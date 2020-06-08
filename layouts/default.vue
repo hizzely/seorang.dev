faaf49939811ead78a8fc13d330634eeefb91575
Fajar Ru <kzofajar@gmail.com>
2020-06-08 09:28:48 +0700

    Extract footer into component

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,19 @@
   <div>
     <Navbar />
     <nuxt />
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
   import Navbar from '~/components/Navbar.vue'
+  import Footer from '~/components/Footer.vue'
 
   export default Vue.extend({
     components: {
-      Navbar
+      Navbar,
+      Footer
     }
   })
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -43,18 +43,5 @@
         </section>
       </div>
     </div>
-
-    <footer class="container text-center text-sm mx-auto p-8 px-8 md:px-0 lg:w-2/3 xl:max-w-3xl">
-      <div class="space-x-2 mb-4">
-        <a href="mailto:bukan@seorang.dev" target="_blank"><img class="w-8 opacity-50 inline-block" alt="Email" src="https://www.iconninja.com/files/141/940/985/gmail-icon.svg"></a>
-        <a href="https://linkedin.com/in/rfajar" target="_blank"><img class="w-8 opacity-50 inline-block" alt="LinkedIn" src="https://www.iconninja.com/files/614/682/533/linkedin-icon.svg"></a>
-        <a href="https://github.com/hizzely" target="_blank"><img class="w-8 opacity-50 inline-block" alt="GitHub" src="https://www.iconninja.com/files/155/576/1004/github-icon.svg"></a>
-        <a href="https://instagram.com/hizzely" target="_blank"><img class="w-8 opacity-50 inline-block" alt="Instagram" src="https://www.iconninja.com/files/757/387/759/instagram-icon.svg"></a>
-        <a href="https://hoobist.com/youtube" target="_blank"><img class="w-8 opacity-50 inline-block" alt="Youtube" src="https://www.iconninja.com/files/92/645/294/youtube-icon.svg"></a>
-      </div>
-      <span class="text-gray-600">&copy; 2020 <span class="font-semibold">Rohmad Fajarudin</span>. Built with Nuxt.js &amp; Tailwind.</span>
-    </footer>
-
-
   </div>
 </template>

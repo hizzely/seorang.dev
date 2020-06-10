424738be72d5b769b00eaa62ac7349c7cfa43a48
Fajar Ru <kzofajar@gmail.com>
2020-06-10 21:21:16 +0700

    Updated navbar to use nuxt-link

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,15 +2,15 @@
   <nav class="bg-white" role="navigation">
     <div class="container py-8 mx-auto px-8 md:px-0 lg:w-2/3 xl:max-w-3xl flex flex-wrap items-center justify-between">
       <div id="navbar-brand" class="mr-4 mb-4 text-center w-full block md:mb-0 md:text-left md:w-auto md:inline-block">
-        <a href="/">
+        <nuxt-link to="/">
           <img src="https://avatars3.githubusercontent.com/u/24291960?s=64" class="inline-block w-8 rounded-full mr-1">
           <span class="text-lg text-gray-700 font-semibold align-middle">Rohmad Fajarudin</span>
-        </a>
+        </nuxt-link>
       </div>
       <div class="text-gray-700 text-center w-full block md:text-left md:w-auto md:inline-block">
-        <a href="/about" class="px-4">About</a>
-        <a href="/snippets" class="px-4">Snippets</a>
-        <a href="/blog" class="px-4">Tulisan</a>
+        <nuxt-link to="/about" class="px-4">About</nuxt-link>
+        <nuxt-link to="/snippets" class="px-4">Snippets</nuxt-link>
+        <nuxt-link to="/blog" class="px-4">Tulisan</nuxt-link>
         <a href="#" class="px-4 border border-gray-400 rounded-md py-1">Portfolio</a>
       </div>
     </div>

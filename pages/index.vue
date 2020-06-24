95874c1deec0e7b43cc084efb19710cd0db367b6
Fajar Ru <kzofajar@gmail.com>
2020-06-24 11:01:29 +0700

    Fixed lint warning caused by redundant properties

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@
             <nuxt-link to="/snippets" class="inline-block text-xs px-4 py-1 rounded-md border border-gray-400">Semua Snippets</nuxt-link>
           </div>
           <div class="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
-            <nuxt-link :to="snippet.path" v-for="(snippet, i) in snippets" :key="i" class="md:w-1/3 h-32 bg-white shadow-md hover:shadow-lg border rounded-lg inline-block flex items-center justify-center">
+            <nuxt-link :to="snippet.path" v-for="(snippet, i) in snippets" :key="i" class="md:w-1/3 h-32 bg-white shadow-md hover:shadow-lg border rounded-lg flex items-center justify-center">
               <div class="text-center p-4">
                 <img class="inline-block w-8 mb-2" alt="Laravel" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/120px-Laravel.svg.png">
                 <h3 class="text-sm">{{ snippet.title }}</h3>
@@ -33,7 +33,7 @@
             <nuxt-link to="/blog" class="inline-block text-xs px-4 py-1 rounded-md border border-gray-400">Semua Tulisan</nuxt-link>
           </div>
           <div class="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
-            <div v-for="(post, i) in posts" :key="i" class="md:w-1/3 h-48 bg-white shadow-md hover:shadow-lg border rounded-lg inline-block flex items-center justify-center">
+            <div v-for="(post, i) in posts" :key="i" class="md:w-1/3 h-48 bg-white shadow-md hover:shadow-lg border rounded-lg flex items-center justify-center">
               <div class="text-center p-4">
                 <h3 class="text-xl mb-4">{{ post.title }}</h3>
                 <nuxt-link :to="post.path" class="inline-block text-xs px-4 py-1 rounded-md border border-gray-500">Selengkapnya</nuxt-link>
